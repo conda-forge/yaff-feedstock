4937e9a0c0d974941d1d8ffe1152d2b89671e34e
Jan Janssen <jan-janssen@users.noreply.github.com>
2023-01-03 03:14:32 -0700

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,5 @@
-bash -lc "%RECIPE_DIR%\build.sh"
+echo "__version__ = \"%PKG_VERSION%\"" > %SRC_DIR%/yaff/version.py
+%PYTHON% -m pip install . -vv
 
 if %errorlevel% neq 0 exit /b %errorlevel%
 exit /b 0
